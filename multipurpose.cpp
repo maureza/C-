e51a3e1e8cefe818a85e517e6a91f4eda687b185
rezamar555 <rezamar555@gmail.com>
2021-03-05 23:07:52 -0600

    First update

---
+++ multipurpose.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<wchar.h>
+#include<locale.h>
+#include<stdlib.h>
+using namespace std;
+
+int sum()
+{
+
+
+}
+
+
+
+main()
+{
+    setlocale(LC_ALL, "");
+
+    int opt, next;
+    cout << "\t\t***Bienvenido al programa multiusos***\n" << endl;
+    cout << "1.-Operaciones numericas" << endl;
+    cout << "2.-Operaciones con arreglos" << endl;
+    cout << "3.-Ejercicios variados" << endl;
+    cout << "4.-Funcionalidades avanzadas" << endl;
+    cout << "Escoge tu opción" << endl;
+    cin >> opt;
+
+    if(opt != 0)
+    {
+        cout << "\n\nIngresa sólo números" << endl;
+    }
+
+    switch(opt)
+    {
+    case 1:
+        cout << "Qué operación deseas realizar?" << endl;
+        cout << "1.-Suma" << endl;
+        cout << "2.-Resta" << endl;
+        cout << "3.-Division" << endl;
+        cout << "4.-Primo o no" << endl;
+        cout << "5.-Entero o no" << endl;
+        cout << "Presione cualquier tecla para el siguiente apartado..." << endl;
+
+        cin >> next;
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    }
+
+    return 5;
+}
